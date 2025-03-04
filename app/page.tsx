894fc5504d76fad8aaf1c966839d77a7d2122455
Tom Belmans <r0930357@student.thomasmore.be>
2025-03-04 20:09:59 +0100

    colorfull hero section

--- app/page.tsx
+++ app/page.tsx
@@ -48,11 +48,10 @@ export default function Page() {
           <div className="mx-auto max-w-4xl">
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
-                <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-                  {t("fullStackDeveloper")}
-                </h1>
+                <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span
+                    className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">{t('fullStackDeveloper')}</span></h1>
                 <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
-                  {t("heroDescription")}
+                  {t('heroDescription')}
                 </p>
               </div>
               <div className="space-x-4">
